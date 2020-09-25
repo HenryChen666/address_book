0b1ff38574a82811f4f91f8a00228cd8d1e2c269
Henrychen666 <henryc00183001@gmail.com>
2020-09-24 20:32:13 -0400

    inplment few forms

--- src/app/address-list/address-entry.ts
+++ src/app/address-list/address-entry.ts
@@ -4,13 +4,21 @@ export class AddressEntry {
   public phone?: string;
   public email?: string;
   public notes?: string;
+  public street?: string;
+  public postal_code?: string;
+  public city?: string;
+  public country?: string;
 
-
-  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, notes?: string) {
+  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, notes?: string, street?: string, postal_code?: string, city?: string, country?: string ) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
     this.email = email;
     this.notes = notes;
+    this.street = street;
+    this.postal_code = postal_code;
+    this.city = city;
+    this.country = country;
+
   }
 }
